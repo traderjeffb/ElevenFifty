ccae9f0d12157e66f29ff95ad63264d06158c670
Jeff Beeson <traderjeffb@gmail.com>
2017-11-14 15:57:11 -0500

    configured Dbcontext

--- ElevenNote/ElevenNote.Data/IdentityModels.cs
+++ ElevenNote/ElevenNote.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -28,6 +30,31 @@ namespace ElevenNote.Web.Data
         public static ElevenNoteDbContext Create()
         {
             return new ElevenNoteDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                 .Configurations
+                     .Add(new IdentityUserLoginConfiguration())
+                     .Add(new IdentityUserRoleConfiguration());
+        }
+    //TODO  Dave had an extra curly brace here.......................................................
+        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+        {
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
+        }
+
+        public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+        {
+            public IdentityUserRoleConfiguration()
+            {
+                HasKey(iur => iur.RoleId);
+            }
         }
     }
 }
