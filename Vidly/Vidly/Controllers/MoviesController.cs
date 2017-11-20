712b1cca17393fd28e1538ac606287d3d220e53c
Jeff Beeson <traderjeffb@gmail.com>
2017-11-20 08:22:58 -0500

    beginning of Vidly app -practice programming

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -13,8 +13,25 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
+
+            var customers = new List<Customer>
+        {
+        new Customer {Name = "Customer 1"},
+        new Customer {Name = "Customer 2"}
+        };
+
             return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+            Customers = customers;
+            };
+
+
         }
+        
+       
+
 
         public ActionResult Edit(int id)
         {
@@ -30,6 +47,8 @@ namespace Vidly.Controllers
             return Content(String.Format("pageIndex ={0} &sortBy={1}", pageIndex, sortBy));
         }
 
+
+        [Route("movies/released/{year}/{month: regex(\\d{2}):range(1.12)}")]
         public ActionResult ByReleaseDate(int year, int month)
             {
             return Content(year + "/" + month);
